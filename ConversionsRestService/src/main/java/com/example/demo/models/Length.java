808e99c69c898d7932eac7fa84447c5efc356a57
Noel Mangwarira <ncmangwarira@gmail.com>
2020-10-16 16:06:02 +0200

    Added code for calculations to the Length class

--- ConversionsRestService/src/main/java/com/example/demo/models/Length.java
+++ ConversionsRestService/src/main/java/com/example/demo/models/Length.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Service;
 @Service
 public class Length {
 
+	private double kilometres;
+	private double miles;
+	
 	public double getKilometres() {
 		return kilometres;
 	}
@@ -17,6 +20,19 @@ public class Length {
 	public void setMiles(double miles) {
 		this.miles = miles;
 	}
-	private double kilometres;
-	private double miles;
+	
+	
+	
+public double calculate_ktom(Length kilometres_ktom){
+		
+		double result_ktom = kilometres_ktom.kilometres * 0.621371;
+		return result_ktom;
+		
+		}
+	public double calculate_mtok(Length miles_mtok){
+		
+		double result_mtok = miles_mtok.miles * 1.60934;
+		return result_mtok;
+		}
+	
 }

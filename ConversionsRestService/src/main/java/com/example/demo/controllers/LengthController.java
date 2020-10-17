c187fb27cd4e56e8296c0b30b04a857b936c16f3
Noel Mangwarira <ncmangwarira@gmail.com>
2020-10-17 10:26:54 +0200

    Added try catch and logging

--- ConversionsRestService/src/main/java/com/example/demo/controllers/LengthController.java
+++ ConversionsRestService/src/main/java/com/example/demo/controllers/LengthController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,23 +13,35 @@ import com.example.demo.models.Length;
 @RestController
 public class LengthController {
 	
-	@Autowired
-	
+	Logger logger = LoggerFactory.getLogger(LengthController.class);
 	
 	@RequestMapping(value="/conversions/mtok", method= RequestMethod.POST)
 	public Length MilecToKilometres(@RequestBody Length len_m)
 	{	
+		try {
 		double result_mtok = len_m.getMiles() * 1.60934;
 		len_m.setKilometres(result_mtok);
+		logger.trace("Start Logging");
 		return len_m;
+			}
+	     catch(Exception e) {
 		
+	    	 return null;
+		}		
 	}
 	
 	@RequestMapping(value="/conversions/ktom", method= RequestMethod.POST)
 	public Length KilometrestoMiles(@RequestBody Length len_k)
 	{
+		try {
 		double result_ktom = len_k.getKilometres() * 0.621371;
 		len_k.setMiles(result_ktom);
+		logger.trace("Start Logging");
 		return len_k;
+	}
+	catch(Exception e) {
+		
+		return null;
+		}
 	}
 }
--- ConversionsRestService/src/main/java/com/example/demo/controllers/TemperatureController.java
+++ ConversionsRestService/src/main/java/com/example/demo/controllers/TemperatureController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controllers;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,24 +13,37 @@ import com.example.demo.models.Temperature;
 @RestController
 public class TemperatureController {
 
-	@Autowired
-	
+
+	Logger logger = LoggerFactory.getLogger(TemperatureController.class);
 	
 	@RequestMapping(value="/conversions/ctok", method= RequestMethod.POST)
 	public Temperature CelciusToKelvin(@RequestBody Temperature temp_c)
 	{		
+		try {
 		double result_ctok = temp_c.getCelcius() + 273.15;
 		temp_c.setKelvin(result_ctok);
+		logger.trace("Start Logging");
 		return temp_c;
-		
-	}
+		}
+		catch(Exception e) {
+			
+			return null;
+			}
+		}			
 	
 	@RequestMapping(value="/conversions/ktoc", method= RequestMethod.POST)
 	public Temperature KelvinToCelcius(@RequestBody Temperature temp_k)
 	{
+		try {
 		double result_ktoc = temp_k.getKelvin() - 273.15;
-		temp_k.setCelcius(result_ktoc);;
+		temp_k.setCelcius(result_ktoc);
+		logger.trace("Start Logging");
 		return temp_k;
-	}
+	     }
+		catch(Exception e) {
+			
+			return null;
+			}
+           }
 	
-}
+		}
